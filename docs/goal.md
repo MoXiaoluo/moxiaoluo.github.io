b4f992cc77494d842971ba0671517c0020ee4d5c
MoXiaoluo <771595316@qq.com>
2023-04-22 20:30:41 +0800

    remove unused files

--- docs/goal.md
+++ docs/goal.md
@@ -7,11 +7,11 @@ sidebar_label: 目的
 
 整理在学习过程中遇到的问题以及对一些知道的感悟都会记录在这里。目前在复习 CSS3， HTML5 以及 JavaScript， 框架方面复习 React, Vue 和相关框架
 
-## CSS3
+## CSS
 
 CSS3
 
-## HTML5
+## HTML
 
 HTML5
 
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,6 +23,7 @@ const config = {
 
   onBrokenLinks: "throw",
   onBrokenMarkdownLinks: "warn",
+  deploymentBranch: "gh-pages",
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
@@ -41,15 +42,13 @@ const config = {
           sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // editUrl:"",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          // editUrl:
-          //   "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          // editUrl:"",
         },
         theme: {
           customCss: require.resolve("./src/css/custom.css"),
@@ -75,7 +74,7 @@ const config = {
             position: "left",
             label: "文档",
           },
-          { to: "/blog", label: "博客", position: "left" },
+          // { to: "/blog", label: "博客", position: "left" },
           {
             href: "https://github.com/MoXiaoluo",
             label: "GitHub",
